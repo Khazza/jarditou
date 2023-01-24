69e16ead22d5204bc4a853e26a4b1db1988bb9fe
Khazza <mahetoquet@gmail.com>
2023-01-24 16:25:38 +0100

    Amélioration exercice de l'évaluation (1 a 4)

--- jarditou/js/evaluation.js
+++ jarditou/js/evaluation.js
@@ -1,4 +1,4 @@
-//--------------- EXERCICE 1 - CALCUL DU NOMBRE DE JEUNES, DE MOYENS ET DE VIEUX ---------------
+// --------------- EXERCICE 1 - CALCUL DU NOMBRE DE JEUNES, DE MOYENS ET DE VIEUX ---------------
 // let under20 = 0; //initialiser le total de personne sous 20 ans à 0
 // let between20and40 = 0; //initialiser le total de personne entre 20 et 40 ans à 0
 // let over40 = 0; //initialiser le total de personne au dessus de 40 ans à 0
@@ -22,7 +22,14 @@
 // console.log(`Il y a ${between20and40} personnes d'âge compris entre 20 et 40 ans.`);
 // console.log(`Il y a ${over40} personnes d'âge supérieur à 40 ans.`);
 
-//--------------- EXERCICE 2 - CALCUL DU NOMBRE DE JEUNES, DE MOYENS ET DE VIEUX ---------------
+
+// ----------------------------------------------------------------------
+// ----------------------------------------------------------------------
+// ----------------------------------------------------------------------
+
+
+
+//--------------- EXERCICE 2 - TABLE DE MULTIPLICATION  ---------------
 // function displayMultiplicationTable() {
 //   let num = prompt(
 //     "Entrez un nombre pour afficher sa table de multiplication:"
@@ -34,7 +41,14 @@
 // }
 // displayMultiplicationTable(); //Affichage de la table
 
+
+// ----------------------------------------------------------------------
+// ----------------------------------------------------------------------
+// ----------------------------------------------------------------------
+
+
 //--------------- EXERCICE 3 - RECHERCHE D'UN PRENOM ---------------
+// -------------------------ESSAI N°1
 // let tab = ["Audrey", "Aurélien", "Flavien", "Jérémy", "Laurent", "Melik", "Nouara", "Salem", "Samuel", "Stéphane"];
 // function deleteName() {
 //   let name = prompt("Entrez un prénom à supprimer:");
@@ -61,8 +75,56 @@
 // deleteName();
 // alert(tab);
 
+// -------------------------ESSAI N°2
+// var tab = ["Audrey", "Aurélien", "Flavien", "Jérémy", "Laurent", "Melik", "Nouara", "Salem", "Samuel", "Stéphane"];
+// console.log(tab);
+// while (true) {
+//   var prenom = prompt("Entrez un prénom à supprimer:");
+//   var index = tab.indexOf(prenom);
+  
+//   if (index === -1) {
+//     alert("Ce prénom n'a pas été trouvé dans le tableau.");
+//   } else {
+//     tab.splice(index, 1);
+//     alert("Le prénom a été supprimé avec succès.");
+//     break;
+//   }
+// }
+// console.log(tab);
+
+// -------------------------ESSAI N°3
+// var tab = ["Audrey", "Aurélien", "Flavien", "Jérémy", "Laurent", "Melik", "Nouara", "Salem", "Samuel", "Stéphane"];
+// console.log(tab);
+// // Boucle pour demander un prénom à supprimer
+// while (true) {
+//    // Demander à l'utilisateur d'entrer un prénom
+//   var prenom = prompt("Entrez un prénom à supprimer:").toLowerCase();
+//    // Trouver l'index du prénom dans le tableau en utilisant la fonction map
+//   var index = tab.map(function(e) { return e.toLowerCase(); }).indexOf(prenom);
+  
+//   // Si le prénom n'a pas été trouvé
+//   if (index === -1) {
+//     alert("Ce prénom n'a pas été trouvé dans le tableau.");
+//   } else {
+//     // Supprimer le prénom en utilisant splice
+//     tab.splice(index, 1);
+//     alert("Le prénom a été supprimé avec succès.");
+//   }
+//   // Si le tableau est vide, relancer la boucle
+//      if(tab.length===0) {
+//         alert("Tableau est vide");
+//         break;
+//     }
+// }
+// console.log(tab);
+
+// ----------------------------------------------------------------------
+// ----------------------------------------------------------------------
+// ----------------------------------------------------------------------
+
+
 //--------------- EXERCICE 4 - TOTAL D'UNE COMMANDE ---------------
-// -------------------------ESSAI N°1 PAS CONCLUANT MAIS FONCTIONNEL-------------------------
+// -------------------------ESSAI N°1 PAS CONCLUANT MAIS FONCTIONNEL (N'arrondi pas)-------------------------
 // let PU = prompt("Saisir le prix unitaire");
 // let QTECOM = prompt("Saisir la quantité commandée");
 // let TOT = PU * QTECOM;
@@ -127,34 +189,40 @@
 // console.log("Frais de port: " + PORT + "€"); // afficher les frais de port
 // console.log("Prix à payer: " + PAP + "€"); // afficher le prix à payer
 
+
+// ----------------------------------------------------------------------
+// ----------------------------------------------------------------------
+// ----------------------------------------------------------------------
+
+
 //--------------- EXERCICE 5 - verification d'un formulaire ---------------
-const form = document.getElementById("formulaire_contact");
+// const form = document.getElementById("formulaire_contact");
 
-// Détecter l'événement d'envoi
-form.addEventListener("submit", function (e) {
-  e.preventDefault();
-  // variable error pour vérifier s'il y a des champs vides
-  let error = false;
+// // Détecter l'événement d'envoi
+// form.addEventListener("submit", function (e) {
+//   e.preventDefault();
+//   // variable error pour vérifier s'il y a des champs vides
+//   let error = false;
 
-  // Récupération de tous les champs
-  let inputs = form.getElementsByTagName("input");
+//   // Récupération de tous les champs
+//   let inputs = form.getElementsByTagName("input");
 
-  // Boucle pour vérifier si les champs sont vides
-  for (let i = 0; i < inputs.length; i++) {
-    if (inputs[i].value === "") {
-      // Ajout de bordure rouge pour indiquer une erreur
-      inputs[i].style.borderColor = "red";
+//   // Boucle pour vérifier si les champs sont vides
+//   for (let i = 0; i < inputs.length; i++) {
+//     if (inputs[i].value === "") {
+//       // Ajout de bordure rouge pour indiquer une erreur
+//       inputs[i].style.borderColor = "red";
 
-      // Changement de la valeur de error à true pour indiquer une erreur
-      error = true;
-    } else {
-      // enlever la bordure rouge si le champ est rempli
-      inputs[i].style.borderColor = "";
-    }
-  }
+//       // Changement de la valeur de error à true pour indiquer une erreur
+//       error = true;
+//     } else {
+//       // enlever la bordure rouge si le champ est rempli
+//       inputs[i].style.borderColor = "";
+//     }
+//   }
 
-  // Empêcher l'envoi du formulaire en cas d'erreur
-  if (error) {
-    e.preventDefault();
-  }
-});
+//   // Empêcher l'envoi du formulaire en cas d'erreur
+//   if (error) {
+//     e.preventDefault();
+//   }
+// });
