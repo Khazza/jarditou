6537a4122b3664b4b32e82a8e45a47f3df5f887c
Khazza <mahetoquet@gmail.com>
2023-01-26 15:29:05 +0100

    Optimisation exo evaluation + début exo eval 5

--- jarditou/js/evaluationExo3.js
+++ jarditou/js/evaluationExo3.js
@@ -1,4 +1,50 @@
 //--------------- EXERCICE 3 - RECHERCHE D'UN PRENOM ---------------
+// -------------------------ESSAI N°5 (en fonction)
+// Déclaration de la fonction supprimerPrenom
+function supprimerPrenom() {
+    // Déclaration du tableau de prénoms
+    var tab = ["Audrey", "Aurélien", "Flavien", "Jérémy", "Laurent", "Melik", "Nouara", "Salem", "Samuel", "Stéphane"];
+    
+    // Afficher le tableau initial sur la page
+    document.getElementById("tableau-initial").innerHTML = "<h3>Tableau initial: </h3><ul>" + tab.map(prenom => "<li>" + prenom + "</li>").join("") + "</ul>";
+    
+    // Boucle pour demander un prénom à supprimer
+    while (true) {
+        // Demander à l'utilisateur d'entrer un prénom
+        var prenom = prompt(tab + "\nEntrez un prénom à supprimer :");
+        
+        // Si l'utilisateur appuie sur annuler (null) on sort de la boucle
+        if(prenom == null){
+            break;
+        }
+        
+        // Mettre tous les prénoms en minuscule
+        prenom = prenom.toLowerCase();
+        
+        // Trouver la place du prénom dans le tableau en utilisant la fonction map pour mettre tous les prénoms en minuscule
+        var index = tab.map(function(e) { return e.toLowerCase(); }).indexOf(prenom);
+        
+        // Si le prénom n'a pas été trouvé
+        if (index === -1) {
+            alert("Ce prénom n'a pas été trouvé dans le tableau.");
+        } else {
+            // Boucle pour décaler les éléments du tableau
+            for (let i = index; i < tab.length - 1; i++) {
+                tab[i] = tab[i + 1];
+            }
+            // Supprimer le dernier élément ou mettre à blanc
+            tab.pop();
+            // Afficher le nouveau tableau sur la page
+            document.getElementById("nouveau-tableau").innerHTML = "<h3>Nouveau tableau: </h3><ul>" + tab.map(prenom => "<li>" + prenom + "</li>").join("") + "</ul>";
+            break;
+        }
+    }
+}
+supprimerPrenom()
+
+// ----------------------------v-v-v-v-v-Anciens essais (plus bas)-v-v-v-v-v-----------------
+// ------------------------------------------------------------------------------------------
+// ------------------------------------------------------------------------------------------
 // -------------------------ESSAI N°1
 // let tab = ["Audrey", "Aurélien", "Flavien", "Jérémy", "Laurent", "Melik", "Nouara", "Salem", "Samuel", "Stéphane"];
 // function deleteName() {
@@ -26,6 +72,9 @@
 // deleteName();
 // alert(tab);
 
+
+// ------------------------------------------------------------------------------------------
+// ------------------------------------------------------------------------------------------
 // -------------------------ESSAI N°2
 // var tab = ["Audrey", "Aurélien", "Flavien", "Jérémy", "Laurent", "Melik", "Nouara", "Salem", "Samuel", "Stéphane"];
 // console.log(tab);
@@ -43,34 +92,84 @@
 // }
 // console.log(tab);
 
+
+// ------------------------------------------------------------------------------------------
+// ------------------------------------------------------------------------------------------
 // -------------------------ESSAI N°3
-// Déclaration du tableau
-var tab = ["Audrey", "Aurélien", "Flavien", "Jérémy", "Laurent", "Melik", "Nouara", "Salem", "Samuel", "Stéphane"];
+// // Déclaration du tableau
+// var tab = ["Audrey", "Aurélien", "Flavien", "Jérémy", "Laurent", "Melik", "Nouara", "Salem", "Samuel", "Stéphane"];
 
-// Afficher le tableau initial sur la page
-document.getElementById("tableau-initial").innerHTML = "<h3>Tableau initial: </h3><ul>" + tab.map(prenom => "<li>" + prenom + "</li>").join("") + "</ul>";
+// // Afficher le tableau initial sur la page
+// document.getElementById("tableau-initial").innerHTML = "<h3>Tableau initial: </h3><ul>" + tab.map(prenom => "<li>" + prenom + "</li>").join("") + "</ul>";
 
-// Boucle pour demander un prénom à supprimer
-while (true) {
-    // Demander à l'utilisateur d'entrer un prénom
-    var prenom = prompt("Audrey, Aurélien, Flavien, Jérémy, Laurent, Melik, Nouara, Salem, Samuel, Stéphane" + "\nEntrez un prénom à supprimer :");
-    // Si l'utilisateur appuie sur annuler (null) on sort de la boucle
-    if(prenom == null){
-        break;
-    }
-    // Mettre tous les prénoms en minuscule
-    prenom = prenom.toLowerCase();
-    // Trouver la place du prénom dans le tableau en utilisant la fonction map pour mettre tous les prénoms en minuscule
-    var index = tab.map(function(e) { return e.toLowerCase(); }).indexOf(prenom);
+// // Boucle pour demander un prénom à supprimer
+// while (true) {
+//     // Demander à l'utilisateur d'entrer un prénom
+//     // var prenom = prompt("Audrey, Aurélien, Flavien, Jérémy, Laurent, Melik, Nouara, Salem, Samuel, Stéphane" + "\nEntrez un prénom à supprimer :");
+//     var prenom = prompt(tab + "\nEntrez un prénom à supprimer :");
+//     // Si l'utilisateur appuie sur annuler (null) on sort de la boucle
+//     if(prenom == null){
+//         break;
+//     }
+//     // Mettre tous les prénoms en minuscule
+//     prenom = prenom.toLowerCase();
+//     // Trouver la place du prénom dans le tableau en utilisant la fonction map pour mettre tous les prénoms en minuscule
+//     var index = tab.map(function(e) { return e.toLowerCase(); }).indexOf(prenom);
 
-    // Si le prénom n'a pas été trouvé
-    if (index === -1) {
-        alert("Ce prénom n'a pas été trouvé dans le tableau.");
-    } else {
-        // Supprimer le prénom en utilisant splice
-        tab.splice(index, 1);
-        // Afficher le nouveau tableau sur la page
-        document.getElementById("nouveau-tableau").innerHTML = "<h3>Nouveau tableau: </h3><ul>" + tab.map(prenom => "<li>" + prenom + "</li>").join("") + "</ul>";
-        break;
-    }
-}
+//     // Si le prénom n'a pas été trouvé
+//     if (index === -1) {
+//         alert("Ce prénom n'a pas été trouvé dans le tableau.");
+//     } else {
+//         // Supprimer le prénom en utilisant splice
+//         tab.splice(index, 1);
+//         // Afficher le nouveau tableau sur la page
+//         document.getElementById("nouveau-tableau").innerHTML = "<h3>Nouveau tableau: </h3><ul>" + tab.map(prenom => "<li>" + prenom + "</li>").join("") + "</ul>";
+//         break;
+//     }
+// }
+
+
+// ------------------------------------------------------------------------------------------
+// ------------------------------------------------------------------------------------------
+// -------------------------ESSAI N°4
+// // Déclaration du tableau de prénoms
+// var tab = ["Audrey", "Aurélien", "Flavien", "Jérémy", "Laurent", "Melik", "Nouara", "Salem", "Samuel", "Stéphane"];
+
+// // Afficher le tableau initial sur la page
+// document.getElementById("tableau-initial").innerHTML = "<h3>Tableau initial: </h3><ul>" + tab.map(prenom => "<li>" + prenom + "</li>").join("") + "</ul>";
+
+// // Boucle pour demander un prénom à supprimer
+// while (true) {
+//     // Demander à l'utilisateur d'entrer un prénom
+//     var prenom = prompt(tab + "\nEntrez un prénom à supprimer :");
+
+//     // Si l'utilisateur appuie sur annuler (null) on sort de la boucle
+//     if(prenom == null){
+//         break;
+//     }
+
+//     // // Normaliser la chaîne de caractères et supprimer les caractères accentués   -------(Marche pas)
+//     // prenom = prenom.normalize("NFD").replace(/[\u0300-\u036f]/g, "");             -------(Marche pas)
+
+//     // Mettre tous les prénoms en minuscule
+//     prenom = prenom.toLowerCase();
+
+//     // Trouver la place du prénom dans le tableau
+//     var index = tab.map(function(e) { return e.toLowerCase(); }).indexOf(prenom);
+
+//     // Si le prénom n'a pas été trouvé
+//     if (index === -1) {
+//         alert("Ce prénom n'a pas été trouvé dans le tableau.");
+//     } else {
+//         // Boucle pour décaler les éléments du tableau
+//         for (let i = index; i < tab.length - 1; i++) {
+//             tab[i] = tab[i + 1];
+//         }
+//         // Supprimer le dernier élément ou mettre à blanc
+//         tab.pop();
+//         // Afficher le nouveau tableau sur la page
+//         document.getElementById("nouveau-tableau").innerHTML = "<h3>Nouveau tableau: </h3><ul>" + tab.map(prenom => "<li>" + prenom + "</li>").join("") + "</ul>";
+//         break;
+//     }
+// }
+
---
+++ jarditou/js/script.js
@@ -0,0 +1,124 @@
+// Récupération des éléments du formulaire
+let form = document.getElementById("formulaire_contact");
+let nom = document.getElementById("nom");
+let prenom = document.getElementById("prenom");
+let genre = document.getElementsByName("genre");
+let date = document.getElementById("date");
+let cp = document.getElementById("cp");
+let adresse = document.getElementById("adresse");
+let ville = document.getElementById("ville");
+let email = document.getElementById("email");
+let sujet = document.getElementById("sujet");
+let commentaire = document.getElementsByName("commentaire")[0];
+let validation = document.getElementsByName("validation")[0];
+
+// Ajout d'un écouteur d'événement sur le formulaire pour lancer la vérification de saisie au submit
+form.addEventListener("submit", function(e) {
+  // Initialisation de la variable de vérification à true (tout est valide par défaut)
+  let formValid = true;
+
+  // Vérification de la saisie du nom
+  if (nom.value.trim() === "") {
+    formValid = false;
+    nom.style.borderColor = "red";
+  } else {
+    nom.style.borderColor = "green";
+  }
+
+  // Vérification de la saisie du prénom
+  if (prenom.value.trim() === "") {
+    formValid = false;
+    prenom.style.borderColor = "red";
+  } else {
+    prenom.style.borderColor = "green";
+  }
+
+  // Vérification de la saisie du genre
+  let genreChecked = false;
+  for (let i = 0; i < genre.length; i++) {
+    if (genre[i].checked) {
+      genreChecked = true;
+      break;
+    }
+  }
+  if (!genreChecked) {
+    formValid = false;
+    for (let i = 0; i < genre.length; i++) {
+      genre[i].parentElement.style.color = "red";
+    }
+  } else {
+    for (let i = 0; i < genre.length; i++) {
+      genre[i].parentElement.style.color = "green";
+    }
+  }
+
+  // Vérification de la saisie de la date
+  if (date.value.trim() === "") {
+    formValid = false;
+    date.style.borderColor = "red";
+  } else {
+    date.style.borderColor = "green";
+  }
+
+  // Vérification de la saisie du code postal
+  if (cp.value.trim() === "") {
+    formValid = false;
+    cp.style.borderColor = "red";
+  } else {
+    cp.style.borderColor = "green";
+  }
+
+  // Vérification de la saisie de l'adresse
+  if (adresse.value.trim() === "") {
+    formValid = false;
+    adresse.style.borderColor = "red";
+} else {
+adresse.style.borderColor = "green";
+}
+
+// Vérification de la saisie de la ville
+if (ville.value.trim() === "") {
+formValid = false;
+ville.style.borderColor = "red";
+} else {
+ville.style.borderColor = "green";
+}
+
+// Vérification de la saisie de l'email
+if (!/^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+.[a-zA-Z]{2,4}$/.test(email.value)) {
+formValid = false;
+email.style.borderColor = "red";
+} else {
+email.style.borderColor = "green";
+}
+
+// Vérification de la saisie du sujet
+if (sujet.value.trim() === "") {
+formValid = false;
+sujet.style.borderColor = "red";
+} else {
+sujet.style.borderColor = "green";
+}
+
+// Vérification de la saisie du commentaire
+if (commentaire.value.trim() === "") {
+formValid = false;
+commentaire.style.borderColor = "red";
+} else {
+commentaire.style.borderColor = "green";
+}
+
+// Vérification de la case de validation
+if (!validation.checked) {
+formValid = false;
+validation.parentElement.style.color = "red";
+} else {
+validation.parentElement.style.color = "green";
+}
+
+// Si la variable formValid est false, on empêche l'envoi du formulaire
+if (!formValid) {
+e.preventDefault();
+alert("Veuillez remplir correctement tous les champs du formulaire.");
+}
+});
